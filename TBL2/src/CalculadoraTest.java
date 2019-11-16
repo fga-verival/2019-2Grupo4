59a43af5dbbe4142037e051e7ce16050ba89776f
Henrike100 <henrikem100@gmail.com>
2019-11-16 17:24:48 -0200

    Caso B - Commit 5

--- TBL2/src/CalculadoraTest.java
+++ TBL2/src/CalculadoraTest.java
@@ -46,16 +46,8 @@ public class CalculadoraTest {
 	}
 
 	@Test
-	public void ImpostoRendaTest1() {
-		Calculadora calc = new Calculadora(60, 1000.00f, 8.5f);
-		double d = 3.14f;
-		assertEquals(calc.calculaImpostoRenda(), d, 0.01);
-	}
-
-	@Test
-	public void ImpostoRendaTest2() {
-		Calculadora calc = new Calculadora(1000, 100.00f, 7.5f);
-		double d = 3.08f;
-		assertEquals(calc.calculaImpostoRenda(), d, 0.01);
+	public void ImpostoRendaTest() {
+		Calculadora calc = new Calculadora(n, p, i);
+		assertEquals(calc.calculaImpostoRenda(), resposta.d, 0.01);
 	}
 }
