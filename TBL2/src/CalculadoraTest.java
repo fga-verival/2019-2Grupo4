0e869dda26bc041a3ca1b5723587a0bcaf0d5e1c
Henrike100 <henrikem100@gmail.com>
2019-11-16 17:18:57 -0200

    Caso A - Commit 3

--- TBL2/src/CalculadoraTest.java
+++ TBL2/src/CalculadoraTest.java
@@ -5,9 +5,16 @@ import org.junit.Test;
 public class CalculadoraTest {
 
 	@Test
-	public void RendimentoBrutoTest() {
+	public void RendimentoBrutoTest1() {
 		Calculadora calc = new Calculadora(60, 1000.00f, 8.5f);
 		double rb = 13.97f;
+		assertEquals(calc.calculaRendimentoBruto(), rb, 0.01);
+	}
+
+	@Test
+	public void RendimentoBrutoTest2() {
+		Calculadora calc = new Calculadora(1000, 100.00f, 7.5f);
+		double rb = 20.55f;
 		assertEquals(calc.calculaRendimentoBruto(), rb, 0.01);
 	}
 }
