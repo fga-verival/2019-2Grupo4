772141087f51cc8c029ed04336bdf4289efb13fa
Henrike100 <henrikem100@gmail.com>
2019-11-16 17:20:44 -0200

    Caso A - Commit 5

--- TBL2/src/CalculadoraTest.java
+++ TBL2/src/CalculadoraTest.java
@@ -1,20 +1,47 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class CalculadoraTest {
-
-	@Test
-	public void RendimentoBrutoTest1() {
-		Calculadora calc = new Calculadora(60, 1000.00f, 8.5f);
-		double rb = 13.97f;
-		assertEquals(calc.calculaRendimentoBruto(), rb, 0.01);
+	int n;
+	double p, i;
+	Resposta resposta;
+	
+	public CalculadoraTest(int n, double p, double i, Resposta resposta) {
+		this.n = n;
+		this.p = p;
+		this.i = i;
+		this.resposta = resposta;
 	}
 
+	@Parameters
+	public static Collection<Object[]> getParameters() {
+		Object[][] respostas = new Object[][] {
+			{60, 1000.0, 8.5, new Resposta(13.97, 3.14, 1.0829)},
+			{120, 500.00, 8.0, new Resposta(13.15, 2.96, 2.0384)},
+			{240, 3000.0, 9.0, new Resposta(177.53, 35.51, 4.7342)},
+			{270, 2000.0, 8.5, new Resposta(125.75, 25.15, 5.0301)},
+			{390, 100.0, 7.5, new Resposta(8.01, 1.40, 6.6113)},
+			{420, 250.00, 8.0, new Resposta(23.01, 4.03, 7.5945)},
+			{550, 400.0, 8.0, new Resposta(48.22, 8.44, 9.9452)},
+			{670, 800.0, 8.0, new Resposta(117.48, 20.56, 12.1151)},
+			{700, 2500.0, 9.0, new Resposta(431.51, 75.51, 14.2397)},
+			{900, 4200.00, 9.5, new Resposta(983.84, 147.58, 19.9110)},
+			{1000, 100.00, 7.5, new Resposta(20.55, 3.08, 17.4658)}
+		};
+		
+		return Arrays.asList(respostas);
+	}
+	
 	@Test
-	public void RendimentoBrutoTest2() {
-		Calculadora calc = new Calculadora(1000, 100.00f, 7.5f);
-		double rb = 20.55f;
-		assertEquals(calc.calculaRendimentoBruto(), rb, 0.01);
+	public void RendimentoBrutoTest() {
+		Calculadora calc = new Calculadora(n, p, i);
+		assertEquals(calc.calculaRendimentoBruto(), resposta.rb, 0.01);
 	}
 }
---
+++ TBL2/src/Resposta.java
@@ -0,0 +1,10 @@
+
+public class Resposta {
+	public double rb, d, rl;
+	
+	public Resposta(double rb, double d, double rl) {
+		this.rb = rb;
+		this.d = d;
+		this.rl = rl;
+	}
+}
